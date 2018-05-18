01e274b03f8cfc51e1c41f82914219a05d6a38c0
sebasaade <sebasaade@gmail.com>
2018-05-17 21:39:56 -0300

    Mensaje

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import include, url
 from . import views
+<<<<<<< HEAD
+from .views import search
+=======
+>>>>>>> b3618b8a125e1092767ae066f7ae983f48e8b0e0
 
 urlpatterns = [
     url(r'^$', views.post_list, name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name= 'post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+<<<<<<< HEAD
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name= 'post_edit'),
+    url(r'^post/search/$', views.search, name='search'),
+=======
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name= 'post_edit')
+>>>>>>> b3618b8a125e1092767ae066f7ae983f48e8b0e0
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+<<<<<<< HEAD
+from django.shortcuts import render_to_response
+from django.db.models import Q
+
+    
+=======
 
 
+>>>>>>> b3618b8a125e1092767ae066f7ae983f48e8b0e0
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
@@ -42,3 +49,22 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+<<<<<<< HEAD
+
+def search(request):
+    query = request.GET.get('q', '')
+    if query:
+        qset = (
+            Q(title__icontains=query) |
+            Q(text__icontains=query) |
+            Q(published_date__icontains=query) 
+        )
+        results = Post.objects.filter(qset).distinct()
+    else:
+        results = []
+    return render_to_response("blog/base.html", {
+        "results": results,
+        "query":query
+    })
+=======
+>>>>>>> b3618b8a125e1092767ae066f7ae983f48e8b0e0
